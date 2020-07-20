61b73de5efbe0bd64642728aaf038ed0df07822b
Ali <mojtabavi99@yahoo.com>
2020-07-20 16:59:07 +0430

    React-Spread Operators

--- React/react-app/src/index.js
+++ React/react-app/src/index.js
@@ -1,3 +1,18 @@
-const colors = ["red","green","blue"];
-const items = colors.map(color =>`<li>${color}</li>`)
-console.log(items)
+let first = [1,2,3];
+let second = [4,5,6];
+
+// Defualt Method
+// const combined = first.concat(second);
+
+let combined = [...first,'a',...second,'b'];
+console.log("combined",combined);
+
+// Print: combined > Array(8) [ 1, 2, 3, "a", 4, 5, 6, "b" ]
+
+const clone = [...first];
+console.log(clone);
+
+first = {name: 'Ali'};
+second = {job: 'instructur'};
+combined = {...first,...second,location: 'Austria'};
+console.log("combined",combined);
