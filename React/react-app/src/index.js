bfde37522c1e430eb13cbb5b383d3ba88e94b046
Ali <mojtabavi99@yahoo.com>
2020-07-20 21:59:44 +0430

    React-classes

--- React/react-app/src/index.js
+++ React/react-app/src/index.js
@@ -1,18 +1,13 @@
-let first = [1,2,3];
-let second = [4,5,6];
 
-// Defualt Method
-// const combined = first.concat(second);
+class Person {
+    constructor(name){
+        this.name = name;
+    }
+    
+    walk() {
+        console.log("walk")
+    }
+}
 
-let combined = [...first,'a',...second,'b'];
-console.log("combined",combined);
 
-// Print: combined > Array(8) [ 1, 2, 3, "a", 4, 5, 6, "b" ]
-
-const clone = [...first];
-console.log(clone);
-
-first = {name: 'Ali'};
-second = {job: 'instructur'};
-combined = {...first,...second,location: 'Austria'};
-console.log("combined",combined);
+const person = new Person('Mosh');
