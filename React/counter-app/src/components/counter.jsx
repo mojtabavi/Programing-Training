19186d182c0d9c55cd66923aa84da8df339d673e
Ali <mojtabavi99@yahoo.com>
2020-07-21 00:26:40 +0430

    React-Rendering Classes Dynamically Refactor

--- React/counter-app/src/components/counter.jsx
+++ React/counter-app/src/components/counter.jsx
@@ -9,17 +9,21 @@ class Counter extends Component {
   //   styles = { fontSize: 50, fontWeight: "bold" };
 
   render() {
-    let classes = "badge m-2 badge-";
-    classes += this.state.count === 0 ? "warning" : "primary";
     return (
       <div>
         {/* <img src={this.state.imageUrl} alt=""></img> */}
-        <span style={{ fontSize: 15 }} className={classes}>
+        <span style={{ fontSize: 15 }} className={this.getBadgeClasses()}>
           {this.formatCount()}
         </span>
         <button className="btn btn-secondary btn-sm">Increament</button>
       </div>
     );
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
   }
 
   formatCount() {
