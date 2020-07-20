d19648c79784e12caa4d2a8a61857935c61df1f3
Ali <mojtabavi99@yahoo.com>
2020-07-20 15:57:01 +0430

    React-javascript Arrow Functions-Filter example

--- React/react-app-1/src/index.js
+++ React/react-app-1/src/index.js
@@ -1,9 +1,22 @@
-// const square = function (number) {
-//   return number * number;
-// };
+const jobs = [{
+    id: 1,
+    isActive: true
+  },
+  {
+    id: 2,
+    isActive: false
+  },
+  {
+    id: 3,
+    isActive: true
+  }
+];
 
+// actual way
+// const activeJobs = jobs.filter(function (job) {
+//   return job.isActive;
+// });
 
+// arrow function way
 
-
-const square = (number) => number * number;
-console.log(square(5));
+const activeJobs = jobs.filter(job => job.isActive);
