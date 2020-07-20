2651eca33cb3586336412ff77509f0df99cb47a3
Ali <mojtabavi99@yahoo.com>
2020-07-20 23:40:08 +0430

    React-function in component

--- React/counter-app/src/components/counter.jsx
+++ React/counter-app/src/components/counter.jsx
@@ -1,13 +1,23 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
+  state = {
+    count: 0,
+  };
   render() {
     return (
       <React.Fragment>
-        <h1>Hello World</h1>
+        <span>{this.formatCount()}</span>
         <button>Increament</button>
       </React.Fragment>
     );
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    // return this.state.count === 0 ? "Zero" : this.state.count;
+    // return count === 0 ? <h1>Zero</h1> : count;
+    return count === 0 ? "Zero" : count;
   }
 }
 
