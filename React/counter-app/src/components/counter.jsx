9a8984a219361532c7b6984f22d46fded27e12a5
Ali <mojtabavi99@yahoo.com>
2020-07-21 19:01:04 +0430

    React-Passing data Props

--- React/counter-app/src/components/counter.jsx
+++ React/counter-app/src/components/counter.jsx
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 0,
+    value: this.props.value,
     // tags: ["tag1", "tag2", "tag3"],
     // imageUrl: "https://picsum.photos/200",
   };
@@ -18,7 +18,7 @@ class Counter extends Component {
   handleIncrement = (product) => {
     // this.state.count++;
     console.log(product);
-    this.setState({ count: this.state.count + 1 });
+    this.setState({ value: this.state.value + 1 });
   };
 
   // doHandleIncrement = () => {
@@ -41,12 +41,12 @@ class Counter extends Component {
 
   getBadgeClasses() {
     let classes = "badge m-2 badge-";
-    classes += this.state.count === 0 ? "warning" : "primary";
+    classes += this.state.value === 0 ? "warning" : "primary";
     return classes;
   }
 
   formatCount() {
-    const { count } = this.state;
+    const { value: count } = this.state;
     // return this.state.count === 0 ? "Zero" : this.state.count;
     // return count === 0 ? <h1>Zero</h1> : count;
     return count === 0 ? "Zero" : count;
