2d3631a77bc63215f79bf1a80aa49c30923471d2
Ali <mojtabavi99@yahoo.com>
2020-07-21 12:37:31 +0430

    JavaScript-Error Handling

---
+++ JavaScript/ERROR-HANDLING/app.js
@@ -0,0 +1,18 @@
+// const getPrice = (amount) => {
+//     if (typeof amount === 'number') {
+//         return amount
+//     } else {
+//         throw Error('Amount must be a number')
+//     }
+// }
+
+// console.log(getPrice('20'))
+
+
+try {
+    const number = 20;
+    console.log(numbers);
+
+} catch (e) {
+    console.log('Error');
+}
