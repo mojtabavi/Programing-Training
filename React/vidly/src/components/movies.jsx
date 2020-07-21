68893b74ab6aad7a4303d8d6ee0f318d48fed872
Ali <mojtabavi99@yahoo.com>
2020-07-21 17:19:08 +0430

    React-Handle Delete Button

--- React/vidly/src/components/movies.jsx
+++ React/vidly/src/components/movies.jsx
@@ -5,6 +5,12 @@ class Movies extends Component {
     state = {   
         movies: getMovies()
       }
+
+    handleDelete = (movie) => {
+        const movies = this.state.movies.filter(m => m._id !== movie._id)
+        this.setState({movies: movies})
+    };
+
     render() { 
         return ( <table className="table">
             <thead>
@@ -13,20 +19,23 @@ class Movies extends Component {
                     <th>Genre</th>
                     <th>Stock</th>
                     <th>Rate</th>
+                    <th></th>
                 </tr>
             </thead>
             <tbody>
                 {this.state.movies.map(movie => (
-                <tr>
+                <tr key={movie._id}>
                     <td>{movie.title}</td>
                     <td>{movie.genre.name}</td>
                     <td>{movie.numberInStock}</td>
                     <td>{movie.dailyRentalRate}</td>
+                    <td><button onClick={() => this.handleDelete(movie)} className="btn btn-danger btn-sm">Delete</button></td>
                 </tr>
 
                 ))}
             </tbody>
-        </table> );
+        </table> 
+        );
     }
 }
  
