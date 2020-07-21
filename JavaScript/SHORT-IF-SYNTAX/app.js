7a0e97b251d4c388b3ca0c23847029549cf44fe8
Ali <mojtabavi99@yahoo.com>
2020-07-21 11:51:43 +0430

    JavaScript-Short if Syntax

---
+++ JavaScript/SHORT-IF-SYNTAX/app.js
@@ -0,0 +1,14 @@
+const price = 79;
+// let message = '';
+
+//Default if syntax
+
+// if (price > 59) {
+//     message = 'Has Discount!';
+// } else {
+//     message = 'Has not Discount!'
+// }
+// console.log(message);
+
+const message = (price > 59) ? 'Has Discount!' : 'Has not Discount!';
+console.log(message);
