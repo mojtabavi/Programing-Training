7f94fc2ed7ac533b16dc3eff48938017ede5ddfb
Ali <mojtabavi99@yahoo.com>
2020-07-21 23:20:02 +0430

    React-Delete component (update components)

--- React/counter-app/src/components/counter.jsx
+++ React/counter-app/src/components/counter.jsx
@@ -36,7 +36,7 @@ class Counter extends Component {
         >
           Increment
         </button>
-        <button onClick={this.props.onDelete} className="btn btn-danger m-2">Delete</button>
+        <button onClick={() => this.props.onDelete(this.props.id)} className="btn btn-danger m-2">Delete</button>
       </div>
     );
   }
--- React/counter-app/src/components/counters.jsx
+++ React/counter-app/src/components/counters.jsx
@@ -11,8 +11,9 @@ class Counters extends Component {
         ]
      }
 
-     handleDelete = () => {
-        console.log("Event Handler Called")
+     handleDelete = (counterId) => {
+        const counters = this.state.counters.filter(c => c.id !== counterId);
+        this.setState({counters: counters})
      };
 
 
