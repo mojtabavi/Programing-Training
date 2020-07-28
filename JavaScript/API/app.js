f338da2e67efa9565b9c65604b826c11c6b3300c
Ali <mojtabavi99@yahoo.com>
2020-07-28 10:57:04 +0430

    JavaScript-XMLHttpRequest post data

--- JavaScript/API/app.js
+++ JavaScript/API/app.js
@@ -1,19 +1,22 @@
 const getBtn = document.querySelector('#get-btn')
 const postBtn = document.querySelector('#post-btn')
 
-const sendHttpRequest = (method,url) => {
+const sendHttpRequest = (method,url,data) => {
     const promise = new Promise((resolve,reject) => {
 
     const xhr = new XMLHttpRequest()
     xhr.open(method,url)
     xhr.responseType = 'json'
+    if(data){
+        xhr.setRequestHeader('Content-Type','application/json')
+    }
     xhr.onload = () => {
         resolve(xhr.response)
     }
     xhr.onerror = () => {
         reject('Error')
     }
-    xhr.send()
+    xhr.send(JSON.stringify(data))
 
     })
     return promise
@@ -34,9 +37,19 @@ const getData = () => {
 }
 
 const postData = () => {
+    sendHttpRequest('POST','https://jsonplaceholder.ir/todos',{
+        userId: 2,
+        id: 20,
+        title: 'تست ۲',
+        completed: false
+    }).then(res => {
+        console.log(res)
+    }).catch(err => {
+        console.log(err)
+    })
 
 }
 
 
 getBtn.addEventListener('click',getData)
-postBtn.addEventListener('click',postBtn)
+postBtn.addEventListener('click',postData)
