2a186fb838333ed6bedb6ff78a2e6ce0d7b70025
Ali <mojtabavi99@yahoo.com>
2020-07-28 10:09:37 +0430

    JavaScript-Async-Await Started

---
+++ JavaScript/Async-await/app.js
@@ -0,0 +1,45 @@
+const products = [{
+    title: 'Book 1',
+    price: 79
+},
+{
+    title: 'Book 2',
+    price: 29
+},
+{
+    title: 'Book 3',
+    price: 59
+}]
+
+
+getProduct = () => {
+    setTimeout(() => {
+        const fetchProduct = products.map((item) => {
+            return `Product: ${item.title} - Price: ${item.price}`
+        })
+        console.log(fetchProduct)
+    },2000)
+}
+
+
+createProduct = () => {
+    return new Promise((resolve,reject) => {
+
+        setTimeout(() => {
+            products.push({
+                title: 'New Book',
+                price: 78
+    
+            })
+            const error = false
+        if(!error){
+            resolve()
+        }else{
+            reject('Error')
+        }
+        },3000) 
+    })
+    
+}
+
+
