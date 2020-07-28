f6fcde0c0c8212c30c32c21e0c1ba6a8e590d2e6
Ali <mojtabavi99@yahoo.com>
2020-07-28 10:49:41 +0430

    JavaScript-XMLHttpRequest add Promise

--- JavaScript/API/app.js
+++ JavaScript/API/app.js
@@ -1,16 +1,35 @@
 const getBtn = document.querySelector('#get-btn')
 const postBtn = document.querySelector('#post-btn')
 
+const sendHttpRequest = (method,url) => {
+    const promise = new Promise((resolve,reject) => {
+
+    const xhr = new XMLHttpRequest()
+    xhr.open(method,url)
+    xhr.responseType = 'json'
+    xhr.onload = () => {
+        resolve(xhr.response)
+    }
+    xhr.onerror = () => {
+        reject('Error')
+    }
+    xhr.send()
+
+    })
+    return promise
+
+}
+
 
 
 const getData = () => {
-    const xhr = new XMLHttpRequest()
-    xhr.open('GET','https://jsonplaceholder.ir/posts/1')
-    xhr.responseType = 'json'
-    xhr.onload = () => {
-        console.log(xhr.response)
-    }
-    xhr.send()
+    sendHttpRequest('GET','https://jsonplaceholder.ir/posts/1')
+    .then(res => {
+        console.log(res)
+    })
+    .catch(err => {
+        console.log(err)
+    })
 
 }
 
