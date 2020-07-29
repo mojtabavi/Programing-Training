62a3c1da65eb5dfa202f908bc77b947c5b335699
Ali <mojtabavi99@yahoo.com>
2020-07-29 09:21:19 +0430

    React-Stateless component function

--- React/counter-app/src/components/navbar.jsx
+++ React/counter-app/src/components/navbar.jsx
@@ -1,16 +1,14 @@
 import React, { Component } from 'react';
 
-class NavBar extends Component {
-    state = {  }
-    render() { 
+const NavBar = (props) => {
         return ( 
             <nav className="navbar navbar-light bg-light">
                 <a className="navbar-brand" href="#">
-        Navbar <span className="badge badge-pill badge-secondary">{this.props.totalCounters}</span>
+        Navbar <span className="badge badge-pill badge-secondary">{props.totalCounters}</span>
                 </a>
             </nav>
          );
     }
-}
+
  
 export default NavBar;
