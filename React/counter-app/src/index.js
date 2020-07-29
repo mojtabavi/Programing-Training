ed51fa2ec088118e09b03209fa3ecf43b0989bc3
Ali <mojtabavi99@yahoo.com>
2020-07-29 09:06:31 +0430

    React-Add Navbar and some change for multiple component in app sync

--- React/counter-app/src/index.js
+++ React/counter-app/src/index.js
@@ -4,10 +4,10 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.css';
-import Counters from "./components/counters";
 
 
-ReactDOM.render( < Counters / > ,
+
+ReactDOM.render( < App / > ,
   document.getElementById('root')
 );
 
