0b03710f27a288928fc7ec95c997629bb1df4b6c
Ali <mojtabavi99@yahoo.com>
2020-07-29 09:33:15 +0430

    React-Distructing Argument-this.props

--- React/counter-app/src/components/counters.jsx
+++ React/counter-app/src/components/counters.jsx
@@ -4,12 +4,13 @@ import Counter from "./counter";
 class Counters extends Component {
 
     render() { 
+        const {onReset,counters,onDelete,onIncrement} = this.props
         return ( 
         <div>
-            <button className="btn btn-primary m-2" onClick={this.props.onReset}>Reset</button>
-            {this.props.counters.map(counter => <Counter
-            onDelete={this.props.onDelete} 
-            onIncrement= {this.props.onIncrement}
+            <button className="btn btn-primary m-2" onClick={onReset}>Reset</button>
+            {counters.map(counter => <Counter
+            onDelete={onDelete} 
+            onIncrement= {onIncrement}
             key={counter.id} 
             counter={counter} >
             </Counter>)}
