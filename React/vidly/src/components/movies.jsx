8ddc48c41f978b57770246c981bd9064a4f7d1af
Ali <mojtabavi99@yahoo.com>
2020-07-30 12:28:17 +0430

    React-Filtering Adding All Genres

--- React/vidly/src/components/movies.jsx
+++ React/vidly/src/components/movies.jsx
@@ -16,7 +16,9 @@ class Movies extends Component {
 
 
     componentDidMount(){
-        this.setState({movies: getMovies(),genres: getGenres()});
+        const genres = [{name: 'All Genres'}, ...getGenres()]
+
+        this.setState({movies: getMovies(),genres});
     }
 
     handleDelete = (movie) => {
@@ -39,7 +41,7 @@ class Movies extends Component {
     };
 
     handleGenreSelect = genre => {
-        this.setState({selectedGenre: genre})
+        this.setState({selectedGenre: genre, currentPage: 1})
     };
 
 
@@ -53,7 +55,7 @@ class Movies extends Component {
         if(count === 0)
             return (<p>There are no movies in database</p>);
 
-            const filtered = selectedGenre ? allMovies.filter(m => m.genre._id == selectedGenre._id) : allMovies;
+            const filtered = selectedGenre && selectedGenre._id ? allMovies.filter(m => m.genre._id == selectedGenre._id) : allMovies;
 
             const movies = paginate(filtered,currentPage,pageSize);
             
