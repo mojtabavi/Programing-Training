f0d1d08ec1ae51147582990dcab90a2cdc2c3a07
Ali <mojtabavi99@yahoo.com>
2020-07-30 12:17:57 +0430

    React-Filtering Handling Selection

--- React/vidly/src/components/common/listGroup.jsx
+++ React/vidly/src/components/common/listGroup.jsx
@@ -3,12 +3,16 @@ import React, { Component } from 'react';
 
 const ListGroup = props => {
 
-    const {items,textProperty,valueProperty} = props;
+    const {items,textProperty,valueProperty,onItemSelect,selectedItem} = props;
 
     return (
     <ul class="list-group">
         {items.map(item => 
-            <li key={item[valueProperty]} class="list-group-item">{item[textProperty]}</li>
+            <li 
+            key={item[valueProperty]}
+            onClick={() => onItemSelect(item)} 
+            className={item === selectedItem ? "list-group-item active" : "list-group-item"}>
+                {item[textProperty]}</li>
         )}
     </ul>
   );
