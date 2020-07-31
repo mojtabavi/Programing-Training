2913db569327881a86ec31a39e6758e87a831d60
Ali <mojtabavi99@yahoo.com>
2020-07-31 14:37:13 +0430

    React-Sort Raising the Sort Event

--- React/vidly/src/components/movies.jsx
+++ React/vidly/src/components/movies.jsx
@@ -17,7 +17,7 @@ class Movies extends Component {
 
 
     componentDidMount(){
-        const genres = [{name: 'All Genres'}, ...getGenres()]
+        const genres = [{ _id: "" , name: 'All Genres'}, ...getGenres()]
 
         this.setState({movies: getMovies(),genres});
     }
@@ -43,6 +43,11 @@ class Movies extends Component {
 
     handleGenreSelect = genre => {
         this.setState({selectedGenre: genre, currentPage: 1})
+    };
+
+
+    handleSort = path => {
+        console.log(path)
     };
 
 
@@ -76,7 +81,8 @@ class Movies extends Component {
                 <MoviesTable 
                 movies={movies}
                 onLike={this.handleLike}
-                onDelete={this.handleDelete}/>
+                onDelete={this.handleDelete}
+                onSort={this.handleSort}/>
                 <Pagination 
                 itemCount={filtered.length} 
                 pageSize={pageSize}
