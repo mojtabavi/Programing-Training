8e48fafed9e305a30c4f6274ec9b5054c188df4a
Ali <mojtabavi99@yahoo.com>
2020-08-03 11:31:00 +0430

    React-Switch

--- React/router-app/src/App.js
+++ React/router-app/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Route } from "react-router-dom";
+import { Route,Switch } from "react-router-dom";
 import NavBar from "./components/navbar";
 import Products from "./components/products";
 import Posts from "./components/posts";
@@ -14,11 +14,14 @@ class App extends Component {
     return (
       <div>
         <NavBar />
+        
         <div className="content">
+        <Switch>
           <Route path="/products" component={ Products } />
           <Route path="/posts" component={ Posts } />
           <Route path="/admin" component={ Dashboard } />
-          <Route path="/" component={ Home } />
+          <Route path="/" exact component={ Home } />
+        </Switch>
         </div>
       </div>
     );
