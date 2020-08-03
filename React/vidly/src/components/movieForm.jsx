162713ea3e5332ac2081e56c73f42d3ac7b1ecf6
Ali <mojtabavi99@yahoo.com>
2020-08-03 23:30:51 +0430

    React-Vidly add components to projects

---
+++ React/vidly/src/components/movieForm.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const NotFound = () => {
+    return (
+    <ReactFragment>
+    <h1>Movie</h1>
+    <button className="btn btn-primary btn-lg">Save</button>
+    </ReactFragment>  );
+}
+ 
+export default NotFound;
