2302d7cbf36c7e22704884e6a7227718753764f5
Ali <mojtabavi99@yahoo.com>
2020-08-09 07:40:27 +0430

    React-Form Add new movies

--- React/vidly/src/components/common/form.jsx
+++ React/vidly/src/components/common/form.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Joi from 'joi-browser';
 import Input from "./input";
+import Select from "./select";
 
 
 class Form extends Component {
@@ -48,6 +49,21 @@ class Form extends Component {
     data[input.name] = input.value;
     this.setState({ data, errors });
   };
+
+  renderSelect(name,label,options){
+    const {data,errors} = this.state;
+
+    return (
+      <Select 
+        name={name}
+        value={data[name]}
+        label={label}
+        options={options}
+        onChange={this.handleChange}
+        error={errors[name]}
+        />
+    )
+  }
 
   renderButton(label){
     return (
--- React/vidly/src/components/movies.jsx
+++ React/vidly/src/components/movies.jsx
@@ -6,6 +6,7 @@ import ListGroup from "./common/listGroup";
 import Pagination from "./common/pagination";
 import { paginate } from "../utils/paginate";
 import _ from 'lodash';
+import { Link } from "react-router-dom";
 
 class Movies extends Component {
     state = {   
@@ -100,6 +101,13 @@ class Movies extends Component {
                 />
             </div>
             <div className="col">
+                <Link
+                    to="/movies/new"
+                    className="btn btn-primary"
+                    style={{ marginBotton: 20}} 
+                >
+                    New Movie
+                </Link>
 
                 <p>Showing {totalCount} movies in database</p>
                 <MoviesTable 
