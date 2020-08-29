02803f8456ea978fd8cac3f6b1f8e9cdcfe85ef6
Ali <mojtabavi99@yahoo.com>
2020-08-29 12:45:31 +0430

    NodeJS-Handling Delete Request

--- NodeJS/express-demo/index.js
+++ NodeJS/express-demo/index.js
@@ -85,6 +85,17 @@ app.put('api/courses/:id', (req,res) => {
 
 });
 
+
+app.delete("/api/courses/:id", (req, res) => {
+  const course = courses.find((c) => c.id === parseInt(req.params.id));
+  if (!course) res.status(404).send("The course with the given ID was not found");
+
+  const index = courses.indexOf(course);
+  courses.splice(index,1);
+  
+  res.send(course);
+});
+
 const port = process.env.PORT || 3000;
 
 app.listen(port,() => console.log(`Listening on port ${port}...`))
