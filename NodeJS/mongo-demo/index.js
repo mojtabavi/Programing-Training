cb2ce7af7aa1f26146efd4fc1e200728e3f1dab1
Ali <mojtabavi99@yahoo.com>
2020-08-29 16:11:26 +0430

    NodeJS-Schema

--- NodeJS/mongo-demo/index.js
+++ NodeJS/mongo-demo/index.js
@@ -7,3 +7,12 @@ mongoose.connect('mongodb://localhost/playground', {
 })
     .then(() => console.log('Connected to MongoDB ... '))
     .catch(err => console.error('Could not connect to MongoDB ...', err));
+
+
+const courseSchema = new mongoose.Schema({
+    name: String,
+    author: String,
+    tags: [String],
+    date: {type: Date, default: Date.now},
+    isPublished: Boolean
+});
