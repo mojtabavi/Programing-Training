309c9ab6ae1e58be85573b9c9229ad9990201bde
Ali <mojtabavi99@yahoo.com>
2020-08-30 14:10:11 +0430

    Node.JS-Mongo Quering a document

--- NodeJS/mongo-demo/index.js
+++ NodeJS/mongo-demo/index.js
@@ -31,3 +31,13 @@ async function createCourse() {
     console.log(result);
 }
 
+async function getCourse() {
+    const courses = await Course
+        .find({ author: 'Ali'})
+        .limit(10)
+        .sort({ name: 1 })
+        .select({ name: 1, tags: 1 })
+    console.log(courses);
+}
+
+getCourse();
